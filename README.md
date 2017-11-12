e76f75fa4c48f36dae32a7b8053f2aff6101343c
ydarun <yaruncs@gmail.com>
2017-11-12 22:10:38 +0530

    my new website

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-![alt tag](https://raw.github.com/dogfalo/materialize/master/images/materialize.gif)
-===========
+1. meta tags remaining for social twitter, open graph, favicons
+2. introduce site map for this website
+3. scroll based animations
+4. more pre loaders
+5. SEO Optimized
+6. well documented
+7. fix quirky direct message
 
-[![Gitter](https://badges.gitter.im/Join Chat.svg)](https://gitter.im/Dogfalo/materialize?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-Materialize, a CSS Framework based on material design
+# ![alt tag](https://raw.github.com/dogfalo/materialize/master/images/materialize.gif)
 
-### Current Version : v0.97.0
+[![Gitter](https://badges.gitter.im/Join Chat.svg)](https://gitter.im/Dogfalo/materialize?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) Materialize, a CSS Framework based on material design
+
+## Current Version : v0.97.0
 
 ## Sass Requirements:
+
 - Ruby Sass 3.3+, LibSass 0.6+
 
 ## Supported Browsers:
+
 Chrome 35+, Firefox 31+, Safari 7+, IE 10+
 
 ## Changelog
+
 - v0.97.0 (June 21, 2015)
+
   - **Documentation changed to use Official Google Icon web font**
   - **Input errors added**
   - Flicker on Firefox on dropdowns fixed
@@ -24,10 +34,14 @@ Chrome 35+, Firefox 31+, Safari 7+, IE 10+
   - Programmatic control of FAB to open/close added
   - Programmatic control of slider to play/pause added
   - Plus many more bug fixes
+
 - v0.96.1 (April 6, 2015)
+
   - Dropdown Fixes
   - Materialize functions fixed for Meteor
+
 - v0.96.0 (April 1, 2015)
+
   - **Toasts, transitions, scrollfire added under Materialize namespace**
   - **Dropdown is now created as a child of its parent**
   - Collapsibles supports nesting
@@ -40,6 +54,7 @@ Chrome 35+, Firefox 31+, Safari 7+, IE 10+
   - Waves no longer breaks on SVG's
 
 - v0.95.3 (Feb 25, 2015)
+
   - Parallax image loading / responsiveness fixes
   - Date picker supports month/year as dropdown
   - Dismissable collection items
@@ -47,6 +62,6 @@ Chrome 35+, Firefox 31+, Safari 7+, IE 10+
   - Pagination Added
   - ScrollFire fixes
 
-
 ## Contributing
+
 [Please read CONTRIBUTING.md for more information](CONTRIBUTING.md)
--- js/script.js
+++ js/script.js
@@ -1,58 +1,53 @@
-/***************************** document ready *************************/
 $(document).ready(function() {
-    /*****************************   Initialising various components ************************************/
-    $(".button-collapse").sideNav(); //sideNav
-    $('.scrollspy').scrollSpy(); //scrollspy
-    $('.modal-trigger').leanModal();
-    /*************************** header transition *********************/
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 20) {
-            $('header').addClass('fixed');
-            $('header').addClass('z-depth-1');
-            $('header').css({
-                "height": "80px",
-                "background-color": "#FFFFFF"
-            });
-            $('.name h1').css({
-                "margin-top": "-5px",
-                "font-size": "3.8rem"
-            });
-            $('nav').css({
-                "margin-top": "-90px"
-            });
-        } else {
-            $('header').removeClass('fixed');
-            $('header').removeClass('z-depth-1');
-            $('header').css({
-                "height": "150px",
-                "background-color": "transparent"
-            });
-            $('.name h1').css({
-                "margin-top": "32px",
-                "font-size": "4.2rem"
-            });
-            $('nav').css({
-                "margin-top": "-100px"
-            });
-        }
+
+    $(window).load(function() {
+        $('.preloader').remove();
     });
 
-    /******************** SMOOTH SCROLL ***********************/
+    /***************** Header BG Scroll ******************/
+
     $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 10) {
+                $('nav').addClass('nav-fix-height reverse-gradient-bg z-depth-1');
+                $('nav').removeClass('transparent z-depth-0');
+                $('.brand-logo,nav ul').css({
+                    "margin-top": "0px"
+                });
+            } else {
+                $('nav').removeClass('nav-fix-height reverse-gradient-bg z-depth-1');
+                $('nav').addClass('transparent z-depth-0');
+                $('.brand-logo,nav ul').css({
+                    "margin-top": "50px"
+                });
+            }
+        });
+    });
+
+    $('.close').click(function(e) {
+        $('#message').closeModal();
+        $('#request').closeModal();
+    });
+
+    $('.scrollspy').scrollSpy();
+
+    $('.modal-trigger').leanModal();
+
+    /******************  SMOOTH SCROLL**********************/
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
                 if (target.length) {
-                    $('html,body').animate({
+                    $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 2200);
+                    }, 2000);
                     return false;
                 }
             }
         });
     });
 });
-/************************* / document ready **************************/
